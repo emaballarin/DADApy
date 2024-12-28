040935a158fd3d7c1286b3e76d4a39613603439d
Emanuele Ballarin <emanuele@ballarin.cc>
2024-12-28 19:54:14 +0100

    Make pixi-compliant with dadaC dependency from https://pipfury.ballarin.cc/

--- setup.py
+++ setup.py
@@ -118,5 +118,4 @@ setup(
     include_package_data=True,
     package_data={"dadapy": ["_utils/discrete_volumes/*.dat"]},
     install_requires=["dadac"],
-    dependency_links=["git+https://github.com/lykos98/dadaC"],
 )
